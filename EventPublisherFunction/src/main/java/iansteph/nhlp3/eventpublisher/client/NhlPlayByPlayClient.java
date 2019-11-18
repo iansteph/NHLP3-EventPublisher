2cee9257b422c436ee3317fd6d8fbf6b045c1a3f
Ian Stephenson 🦈🏒 <ianp.stephenson@yahoo.com>
2019-11-17 17:05:28 -0800

    Catch and log RestClientExceptions when handling and deserializing the NHL Play-by-Play API response
    - Allows for the GameId that had the issue for easier searching

--- EventPublisherFunction/src/main/java/iansteph/nhlp3/eventpublisher/client/NhlPlayByPlayClient.java
+++ EventPublisherFunction/src/main/java/iansteph/nhlp3/eventpublisher/client/NhlPlayByPlayClient.java
@@ -3,6 +3,7 @@ package iansteph.nhlp3.eventpublisher.client;
 import iansteph.nhlp3.eventpublisher.model.nhl.NhlLiveGameFeedResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -20,15 +21,21 @@ public class NhlPlayByPlayClient {
     }
 
     public NhlLiveGameFeedResponse getPlayByPlayEventsSinceLastProcessedTimestamp(final int gameId, final String lastProcessedTimestamp) {
-        final UriComponents uriComponents = UriComponentsBuilder.newInstance()
-                .scheme("http")
-                .host("statsapi.web.nhl.com")
-                .path("api/v1/game/{gameId}/feed/live/diffPatch")
-                .queryParam("startTimecode", lastProcessedTimestamp)
-                .buildAndExpand(gameId);
-        logger.info(format("Calling NHL Play-by-Play timestamp diff API with lastProcessedTimestamp: %s", lastProcessedTimestamp));
-        final NhlLiveGameFeedResponse response = restTemplate.getForObject(uriComponents.toUri(), NhlLiveGameFeedResponse.class);
-        logger.info(format("NHL Play-by-Play timestamp diff API response: %s", response));
-        return response;
+        try {
+            final UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                    .scheme("http")
+                    .host("statsapi.web.nhl.com")
+                    .path("api/v1/game/{gameId}/feed/live/diffPatch")
+                    .queryParam("startTimecode", lastProcessedTimestamp)
+                    .buildAndExpand(gameId);
+            logger.info(format("Calling NHL Play-by-Play timestamp diff API with lastProcessedTimestamp: %s", lastProcessedTimestamp));
+            final NhlLiveGameFeedResponse response = restTemplate.getForObject(uriComponents.toUri(), NhlLiveGameFeedResponse.class);
+            logger.info(format("NHL Play-by-Play timestamp diff API response: %s", response));
+            return response;
+        } catch (RestClientException e) {
+            logger.error(format("GameId %s | Encountered exception when calling NHL Play-by-Play API and/or deserializing the response",
+                    gameId), e);
+            throw e;
+        }
     }
 }
