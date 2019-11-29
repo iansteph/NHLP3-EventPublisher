627e77c77c6508d5b1b7648231c779d2b3a37b40
Ian Stephenson 🦈🏒 <ianp.stephenson@yahoo.com>
2019-11-28 21:19:08 -0800

    Refactor NHL model to be external dependency instead of defined in this package
    - Refactored the model.nhl subdirectory into its own repository on GitHub and published it as a package to GitHub Packages and is available through Maven. See how here: https://help.github.com/en/github/managing-packages-with-github-packages/configuring-apache-maven-for-use-with-github-packages

--- EventPublisherFunction/src/main/java/iansteph/nhlp3/eventpublisher/model/nhl/common/Handedness.java
+++
@@ -1,9 +0,0 @@
-package iansteph.nhlp3.eventpublisher.model.nhl.common;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public enum Handedness {
-
-    @JsonProperty("R") RIGHT,
-    @JsonProperty("L") LEFT
-}
