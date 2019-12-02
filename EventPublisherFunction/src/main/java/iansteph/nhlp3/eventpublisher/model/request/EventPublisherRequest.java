4922bb29ca44ba30a278d166f23f749bcca57c22
Ian Stephenson 🦈🏒 <ianp.stephenson@yahoo.com>
2019-12-01 20:27:05 -0800

    Add toString(), equals(), and hashCode() to EventPublisherRequest

--- EventPublisherFunction/src/main/java/iansteph/nhlp3/eventpublisher/model/request/EventPublisherRequest.java
+++ EventPublisherFunction/src/main/java/iansteph/nhlp3/eventpublisher/model/request/EventPublisherRequest.java
@@ -1,5 +1,7 @@
 package iansteph.nhlp3.eventpublisher.model.request;
 
+import java.util.Objects;
+
 public class EventPublisherRequest {
 
     private int gameId;
@@ -16,5 +18,25 @@ public class EventPublisherRequest {
 
     public void setGameId(final int gameId) {
         this.gameId = gameId;
+    }
+
+    @Override
+    public String toString() {
+        return "EventPublisherRequest{" +
+                "gameId=" + gameId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EventPublisherRequest that = (EventPublisherRequest) o;
+        return gameId == that.gameId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameId);
     }
 }
